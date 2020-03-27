ff3f94a52eb65c6e4ab0e62a88e74ea1b1b82540
Ambreen System76 <ambreen2006@gmail.com>
2020-03-27 16:20:42 -0400

    Added new charts for compared countries and per capita analysis for Canadian provinces

--- scrape_covid19_data.py
+++ scrape_covid19_data.py
@@ -8,15 +8,17 @@ class DataScraper:
     def __init__(self, url):
         self.url = url
         self.soup = None
+        self.publication_date = None
+        self.data_frame = None
 
     def parse(self, in_memory = False):
-        self.fetchData()
+        self.fetch_data()
         a1 = self.soup.find(id='a1')
         a1.findNext('caption')
         token = 'as of '
 
         caption_info_table = str(a1.findNext('caption').string).strip()
-        pub_date = caption_info_table[caption_info_table.find(token)+len(token):]
+        self.publication_date = caption_info_table[caption_info_table.find(token)+len(token):]
 
         head = a1.find_next('thead')
         column_names_tr = head.tr
@@ -34,10 +36,12 @@ class DataScraper:
             values_parsed.append([values[0], int(values[1].replace(',','')), int(values[2].replace(',','')), int(values[3].replace(',',''))])
         
         if not in_memory:
-            self.write('canada_covid19.csv', 'canada_covid19_meta.csv', values_parsed, columns, pub_date)
+            self.write('canada_covid19.csv', 'canada_covid19_meta.csv', values_parsed, columns, self.publication_date)
 
-        cdata = pd.DataFrame(values_parsed, columns = columns)
-        return cdata, pub_date
+        self.data_frame = pd.DataFrame(values_parsed, columns = columns)
+        
+    def get_parsed_data(self):
+        return self.data_frame, self.publication_date
 
     def write(self, filename, meta_filename, values_parsed, columns, pub_date):
         writer = csv.writer(open(filename, 'w'))
@@ -46,13 +50,14 @@ class DataScraper:
         writer.writerow(columns)
         writer.writerow(values_parsed)    
     
-    def fetchData(self):
+    def fetch_data(self):
         response = requests.get(self.url)
         self.soup = BeautifulSoup(response.text, "html.parser")
 
 if __name__ == "__main__":
     url = "https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html"
     scraper = DataScraper(url)
-    data, current = scraper.parse(in_memory = False)
+    scraper.parse(in_memory = False)
+    data, current = scraper.get_parsed_data()
     print("Publication Date ", current)
     print(data)
